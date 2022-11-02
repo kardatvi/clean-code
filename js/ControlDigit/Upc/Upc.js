47cac9635db8bee43d777d45e1e0e43782c4d057
kardatvi <101497891+kardatvi@users.noreply.github.com>
2022-11-02 15:13:22 +0700

    Update Upc.js

--- js/ControlDigit/Upc/Upc.js
+++ js/ControlDigit/Upc/Upc.js
@@ -1,4 +1,13 @@
 export default function upc (number) {
-    // write your code here
+    var code_even = 1; //четный
+    var code_odd = 3; //нечетный
+    var sum = 0;
+    
+   var n = number.length;
+   for (var i=0; i<n; i++;){
+        var multiplier = i%2 + 1; 
+        var sum = sum + number[i]*(i%2)
+   }
+    return 
 }
 
